b459f5fadc66b1036ecf66f946d9c8fe71ed9e3d
Gunneng <s.gunneng@gmail.com>
2024-05-13 19:35:53 +0200

    just horsing around, learning go

--- bases/web/main.go
+++ bases/web/main.go
@@ -3,32 +3,39 @@ package main
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/wiretick/go-htmx/components/logger"
 )
 
 type Post struct {
-	Id    int
 	Title string
 	Body  string
 }
 
-func (Post) create(title, body string) Post {
-	return Post{
-		Id:    1,
-		Title: title,
-		Body:  body,
+func getPosts(w http.ResponseWriter, r *http.Request) {
+	for _, post := range posts {
+		_, err := fmt.Fprintf(w, "%s\n", post.Body)
+		if err != nil {
+			return
+		}
 	}
+
+	fmt.Fprint(w, "\nAnother line of text\n")
 }
 
-func getPosts(w http.ResponseWriter, r *http.Request) {
-	post := Post.create("hello", "world")
-	fmt.Fprintf(w, "Hello world, from logger: %s", logger.Write("Serving on port 8000"))
-	fmt.Fprint(w, "\nAnother line of text")
+func createPost(w http.ResponseWriter, r *http.Request) {
+	title := r.FormValue("title")
+	body := r.FormValue("body")
+
+	post := Post{Title: title, Body: body}
+	posts = append(posts, post)
+
+	w.WriteHeader(http.StatusSeeOther)
 }
+
+var posts []Post = []Post{{Title: "what", Body: "whats"}, {Title: "another", Body: "body another"}}
 
 func main() {
 	http.HandleFunc("GET /", getPosts)
+	http.HandleFunc("POST /posts", createPost)
 
 	http.ListenAndServe(":8000", nil)
 }

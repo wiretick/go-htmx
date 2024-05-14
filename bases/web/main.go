ddeec0420061bdcce835493e75a2859016b12e7a
Gunneng <s.gunneng@gmail.com>
2024-05-14 16:55:44 +0200

    custom mux? yes pls

--- bases/web/main.go
+++ bases/web/main.go
@@ -2,17 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
 )
 
 type Post struct {
-	Title string
-	Body  string
+	title string
+	body  string
 }
 
 func getPosts(w http.ResponseWriter, r *http.Request) {
 	for _, post := range posts {
-		_, err := fmt.Fprintf(w, "%s\n", post.Body)
+		_, err := fmt.Fprintf(w, "%s\n", post.body)
 		if err != nil {
 			return
 		}
@@ -21,21 +23,43 @@ func getPosts(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "\nAnother line of text\n")
 }
 
+func getPost(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil {
+		return
+	}
+
+	// super safe way of doing things ;)
+	fmt.Fprint(w, posts[id])
+}
+
 func createPost(w http.ResponseWriter, r *http.Request) {
 	title := r.FormValue("title")
 	body := r.FormValue("body")
 
-	post := Post{Title: title, Body: body}
+	post := Post{title: title, body: body}
 	posts = append(posts, post)
 
 	w.WriteHeader(http.StatusSeeOther)
 }
 
-var posts []Post = []Post{{Title: "what", Body: "whats"}, {Title: "another", Body: "body another"}}
+var posts []Post = []Post{
+	{title: "what", body: "whats"},
+	{title: "another", body: "body another"},
+}
 
 func main() {
-	http.HandleFunc("GET /", getPosts)
-	http.HandleFunc("POST /posts", createPost)
+	// Using custom ServeMux makes it possible to have multiple different routers
+	router := http.NewServeMux()
 
-	http.ListenAndServe(":8000", nil)
+	router.HandleFunc("GET /", getPosts)
+	router.HandleFunc("GET /posts/{id}", getPost)
+	router.HandleFunc("POST /posts", createPost)
+
+	server := http.Server{
+		Addr:    ":8000",
+		Handler: router,
+	}
+
+	log.Fatal(server.ListenAndServe())
 }
